2947709d0af6c996eac62c10c49a5f848458ef0a
lemiere <lemiere@lpccaen.in2p3.fr>
2017-07-05 11:14:36 +0200

    [python-lecture] tp_polynome add example and exercice

---
+++ tp_polynome/exemples/script_0.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+# Lemiere Yves
+# Juillet 2017
+# Ceci est un commentaire inutile
+
+print("*********************")
+print("* Welcome in script *")
+print("*********************\n")
+
+
+print("The script's name is %s \n" % __file__)
+
+
+a = 42
+b = 3.14
+c = "It is a mistake to think you can solve any major problems just with potatoes"
+d = True
+
+print (a)
+print (b)
+print (c)
+print (d)
+print ("\n\n")
+
+print ("Value a is equal to  %d"%a)
+print ("Value b is equal to  %d"%b)  # Pour afficher un nombre reel, utilisez %f
+print ("Sentence from The Hitchhiker's Guide to the Galaxy : \n%s"%c)
+
+
+
+# Note
+# %d for decimal
+# %f for float
+# %s for string
+
+
+
+
+
+  
+  
