b10b424a8b29fbc03bf4b16e7252045dc2ad8ee8
lemiere <lemiere@lpccaen.in2p3.fr>
2017-07-08 17:28:48 +0200

    [python-lecture] tp_stat : Finish exemples and exercice

--- tp_stat/exemples/boucle_while.py
+++ tp_stat/exemples/boucle_while.py
@@ -10,3 +10,15 @@ print("* Welcome in boucle_while *")
 print("***************************\n")
 
 
+N = None
+iterator = 0
+
+while N != "42":
+    N = input("Enter the answer to life, the universe and everything")
+    iterator=iterator+1
+
+
+print("You tried %d times"%iterator)
+
+
+
