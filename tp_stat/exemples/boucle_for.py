e7eff043b6ea4db9358d3e4aa4c54eb4b89b5b78
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-09-07 17:32:59 +0200

    Correction ortho + mod. mineures

--- tp_stat/exemples/boucle_for.py
+++ tp_stat/exemples/boucle_for.py
@@ -1,47 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Lemiere Yves
 # Juillet 2017
 
 def main():
-    
+
     debug = True
-    
+
     if debug:
         print("*************************")
         print("* Welcome in boucle_for *")
         print("*************************\n")
-        
-        #Ceci est une liste de chaines de caractères
-        my_list_of_characters = ['Spider-Man','Daredevil','Iron Man','Flash','Wonder Woman']
-        print(my_list_of_characters)
-        
-        
-        #La variable 'hero' prendra tour à tour chaque valeur de la liste my_list_of_characters
+
+        # Ceci est une liste de chaines de caractères:
+        my_list_of_heroes = ['Spider-Man','Daredevil','Iron Man','Flash','Wonder Woman']
+        print(my_list_of_heroes)
+
+
+        # La variable 'hero' prendra tour à tour chaque valeur de la liste my_list_of_heroes:
         iterator = 0
-        for hero in my_list_of_characters:
+        for hero in my_list_of_heroes:
             print (iterator)
             print ("my current hero is {}".format(hero))
             iterator = iterator + 1
-            
-            
-            
+
+
+
         print("Finished with {} heroes ".format(iterator))
-        print("Finished with {} heroes ".format(len(my_list_of_characters)))
-            
-        
-        #Cette boucle ne commence qu'à partir du second élément de la list
+        print("Finished with {} heroes ".format(len(my_list_of_heroes)))
+
+
+        # Cette boucle ne commence qu'à partir du second élément de la liste:
         iterator = 0
-        for hero in my_list_of_characters[2:]:
+        for hero in my_list_of_heroes[2:]:
             print (iterator)
             print ("my current hero is {}".format(hero))
             iterator = iterator + 1
-            
+
         print("Finished with {} heroes ".format(iterator))
-        print("Finished with {} heroes ".format(len(my_list_of_characters)))
-    
+        print("Finished with {} heroes ".format(len(my_list_of_heroes)))
+
         return
 
 
 main()
-
